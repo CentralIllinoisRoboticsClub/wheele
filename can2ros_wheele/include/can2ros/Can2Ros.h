d7f0ed64626a5f8093040cc21386aeb7f5aca2f8
coderkarl <21990134+coderkarl@users.noreply.github.com>
2021-12-01 13:13:35 -0600

    Initial Can2Ros.cpp with only imu published

---
+++ can2ros_wheele/include/can2ros/Can2Ros.h
@@ -0,0 +1,47 @@
+// Copyright 2019 coderkarl. Subject to the BSD license.
+
+#ifndef ImuCAN_H
+#define ImuCAN_H
+
+//ROS Includes
+#include <ros/ros.h>
+#include <can_msgs/Frame.h>
+#include "sensor_msgs/Imu.h"
+#include "sensor_msgs/MagneticField.h"
+#include "geometry_msgs/Vector3.h"
+#include <vector>
+//#include "std_msgs/Float64.h"
+//#include <std_msgs/String.h>
+
+
+class Can2Ros
+{
+    public:
+        Can2Ros();
+        ~Can2Ros();
+        
+    private:
+        int convertCAN(const can_msgs::Frame& frame, int16_t data_out[4], unsigned int nVars); //, unsigned int numBytesPerVar)
+    
+        void canCallback(const can_msgs::Frame& frame);
+
+        ros::NodeHandle nh_;
+        ros::Publisher imu_pub_;
+        ros::Publisher mag_pub_;
+        ros::Publisher heading_pub_;
+        ros::Subscriber can_sub_;
+        
+        sensor_msgs::Imu imu;
+        sensor_msgs::MagneticField magXYZ;
+        sensor_msgs::Imu mag_imu;
+        
+        geometry_msgs::Vector3 accel;
+        //std::vector<double> accel_covar; //What is the right type for ROS float64[9]?? boost::array< >?
+        //std::vector<double> gyro_covar;
+        //std::vector<double> mag_covar;
+        //std::vector<double> quat_covar;
+        
+        float compass_heading;
+};
+
+#endif
---
+++ can2ros_wheele/src/Can2Ros.cpp
@@ -0,0 +1,201 @@
+// Copyright 2019 coderkarl. Subject to the BSD license.
+
+#include <tf/transform_datatypes.h>
+#include "../include/can2ros/Can2Ros.h"
+
+/**********************************************************************
+ * CAN communication to ROS
+ * Requires:
+ * sudo apt install ros-kinetic-ros-opencan
+ * rosrun socketcan_bridge socketcan_bridge_node
+ *   The socket_can bridge turns CAN into /received_messages topic
+ *   The socket_can bridge turns /sent_messages into CAN
+ **********************************************************************/
+
+#define RAW_RC_CAN_ID 0x101
+#define ENCODER_CAN_ID 0x105
+#define BATTERY_CAN_ID 0x140
+#define BUMPER_CAN_ID 0x121
+
+#define GYRO_CAN_ID 0x131
+#define ACCEL_CAN_ID 0x132
+#define COMPASS_CAN_ID 0x133
+
+//Constructor
+Can2Ros::Can2Ros()
+{
+  //Topics you want to publish
+  imu_pub_ = nh_.advertise < sensor_msgs::Imu > ("imu", 10);
+  mag_pub_ = nh_.advertise < sensor_msgs::MagneticField > ("magXYZ", 10);
+  heading_pub_ = nh_.advertise < sensor_msgs::Imu > ("mag_imu", 10);
+
+  //Topic you want to subscribe
+  // leddarCallback will run every time ros sees the topic /received_messages
+  can_sub_ = nh_.subscribe("received_messages", 50, &Can2Ros::canCallback,
+      this); //receive CAN
+
+  accel.x = 0;
+  accel.y = 0;
+  accel.z = 9.81; //NEED TO VERIFY SIGN, FOLLOW ENU CONVENTION
+
+  double accel_var = (4.0 * 1e-3f * 9.80665) * (4.0 * 1e-3f * 9.80665); //(m/s/s)^2
+  imu.linear_acceleration_covariance[0] = accel_var;
+  imu.linear_acceleration_covariance[4] = accel_var;
+  imu.linear_acceleration_covariance[8] = accel_var;
+
+  double gyro_var = (0.06 * 3.14159 / 180.0) * (0.06 * 3.14159 / 180.0); //(rad)^2
+  imu.angular_velocity_covariance[0] = gyro_var;
+  imu.angular_velocity_covariance[4] = gyro_var;
+  imu.angular_velocity_covariance[8] = gyro_var;
+
+  double mag_var = (3.0) * (3.0); //microTesla^2, even though it says use Tesla, they only calculate ratio anyway
+  magXYZ.magnetic_field_covariance[0] = mag_var;
+  magXYZ.magnetic_field_covariance[4] = mag_var;
+  magXYZ.magnetic_field_covariance[8] = mag_var;
+
+  imu.orientation.x = 0.0;
+  imu.orientation.y = 0.0;
+  imu.orientation.z = 0.0;
+  imu.orientation.w = 1.0;
+  imu.orientation_covariance[0] = -1;
+  imu.orientation_covariance[4] = -1;
+  imu.orientation_covariance[8] = -1;
+
+  compass_heading = 0;
+  mag_imu.orientation_covariance[8] = (13 * 3.14159 / 180.)
+      * (13 * 3.14159 / 180.); //yaw about z
+
+  imu.header.frame_id = "imu";
+  mag_imu.header.frame_id = "imu";
+  magXYZ.header.frame_id = "imu";
+
+  //NOTE, the covariances in these messages may be ignored, overwritten in the tools you will use:
+  // https://answers.ros.org/question/248619/about-using-imu_tools-filters/
+  // See robot_localization details
+  //    https://answers.ros.org/question/273372/robot_localization-exploding-covariances-why/
+  // This suggests I do need covariance defined in my input /imu message to robot_localization
+}
+
+//Default destructor.
+Can2Ros::~Can2Ros()
+{
+}
+
+int Can2Ros::convertCAN(const can_msgs::Frame &frame, int16_t data_out[4],
+    unsigned int nVars) //, unsigned int numBytesPerVar)
+{
+  if (nVars > 4)
+    nVars = 4;
+
+  // change float[4] data_out to a std::vector<float>
+  for (unsigned int k = 0; k < nVars; ++k)
+  {
+    data_out[k] = (float) (frame.data[2 * k] << 8 | frame.data[2 * k + 1])
+        - 32768; //0-1; 2-3; 4-5; 6-7
+  }
+  return 0;
+}
+
+void Can2Ros::canCallback(const can_msgs::Frame &frame)
+{
+  //can id
+  uint32_t id = frame.id;
+
+  //canConvert common library function pending...
+  int16_t data_out[4];
+  unsigned int nVars = 4;
+  int res = convertCAN(frame, data_out, nVars);
+
+  // ###### rotating imu 180 deg in this code with x = -x, y = -y... consider applying it in tf between imu and base_link
+  //   Also heading - 3.14159
+
+  switch (id)
+  {
+    case COMPASS_CAN_ID:
+    {
+      //NEED TO HAVE IMU_CALIBRATION_STATUS CAN msg sent from arduino, if no compass cal, do not publish mag_pose
+      //  probably remove heading from this CAN msg, group mag-or-compass cal status with heading into one CAN msg
+      compass_heading = (data_out[3]) / 100.0; //centi-deg to deg
+      mag_imu.header.stamp = frame.header.stamp;
+      geometry_msgs::Quaternion q;
+      q = tf::createQuaternionMsgFromYaw(
+          compass_heading * 3.14159 / 180. - 3.14159);
+      //q.normalize();
+      mag_imu.orientation = q;
+      //tf::quaternionTFToMsg(q,mag_pose.pose.pose.orientation);
+      heading_pub_.publish(mag_imu);
+
+      //update x,y,z order in arduino to match our x,y,z? Or just do it here?
+      magXYZ.header.stamp = frame.header.stamp;
+      magXYZ.magnetic_field.x = -data_out[0] / 100.0; //microTesla
+      magXYZ.magnetic_field.y = -data_out[1] / 100.0;
+      magXYZ.magnetic_field.z = data_out[2] / 100.0;
+
+      mag_pub_.publish(magXYZ);
+      break;
+    }
+    case ACCEL_CAN_ID:
+    {
+      imu.header.stamp = frame.header.stamp;
+      //magXYZ.header.frame_id = 'none';
+      accel.x = -data_out[0] / 100.0; // m/s/s,
+      accel.y = -data_out[1] / 100.0;
+      accel.z = data_out[2] / 100.0;
+      break;
+    }
+    case GYRO_CAN_ID: // publish imu here, sample and hold accel values
+    {
+      imu.header.stamp = frame.header.stamp;
+      imu.angular_velocity.x = -data_out[0] / 100.0 * 3.14159 / 180.; //rad/sec
+      imu.angular_velocity.y = -data_out[1] / 100.0 * 3.14159 / 180.;
+      imu.angular_velocity.z = data_out[2] / 100.0 * 3.14159 / 180.;
+      imu.linear_acceleration = accel;
+      imu_pub_.publish(imu);
+      break;
+    }
+    case RAW_RC_CAN_ID:
+    {
+      int16_t raw_speed_cmd = data_out[0];
+      int16_t raw_steer_cmd = data_out[1];
+      int16_t raw_auto_cmd = data_out[2];
+      break;
+    }
+    case ENCODER_CAN_ID:
+    {
+      int16_t raw_left_enc = data_out[0];
+      int16_t raw_right_enc = data_out[1];
+      int16_t raw_auto_cmd = data_out[2];
+      break;
+    }
+    case BATTERY_CAN_ID: // 0x140
+    {
+      int16_t raw_batt = data_out[0];
+      break;
+    }
+    case BUMPER_CAN_ID: // 0x121
+    {
+      int16_t raw_bump = data_out[0];
+      break;
+    }
+
+  }
+}
+
+int main(int argc, char **argv)
+{
+  //Initiate ROS
+  ros::init(argc, argv, "can2ros");
+
+  Can2Ros compass_can;
+  ROS_INFO("Starting Can2Ros");
+  int loop_rate = 10;
+  ros::Rate rate(loop_rate);
+
+  while (ros::ok())
+  {
+    ros::spinOnce();
+    rate.sleep();
+  }
+
+  return 0;
+}
